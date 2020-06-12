497b7f5d4fb712b19bedbb73b12c1c2f8839f2e7
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2020-06-12 15:36:08 +0200

    cleanup

--- hamburgerMenu/script1.js
+++
@@ -1,6 +0,0 @@
-const arrow= document.querySelector('div.arrow');
-const menu = document.querySelector('div.menu');
-arrow.addEventListener('click', function(){
-   menu.classList.toggle('menu-show');
-   arrow.classList.toggle('menu-show');
-});
