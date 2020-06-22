e96feb9004e367018a3df6c06c88a95cb544b6f7
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2020-06-22 21:51:30 +0200

    Added Footer

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,16 +1,30 @@
 const $doc = $(document);
 const $header = $("header");
 const $main = $("main");
+const $footer= $(".footer");
+const $social_media = $(".social-media");
+
 $doc.on("scroll", function () {
   const scrollPos = $doc.scrollTop();
   const sectionOffset = $main.offset().top;
   const headerHeight = $header.outerHeight();
-  console.log(`zmiana`);
+  const footerOffset = $footer.offset().top;
+
   if (scrollPos < sectionOffset) {
     $header.css({
       opacity: 1 - scrollPos / headerHeight,
       filter: `grayscale(${(2 * scrollPos) / headerHeight}), contrast(0.3);`,
     });
+  }
+
+
+    if (scrollPos+headerHeight > footerOffset) {
+     
+      $social_media.addClass("hide")
+   
+  }
+  else {
+    $social_media.removeClass("hide");
   }
 });
 // SCROLL DOWN 
@@ -24,3 +38,5 @@ arrow.addEventListener("click", (e)=>{
     inline: "nearest",
   });
 });
+
+
