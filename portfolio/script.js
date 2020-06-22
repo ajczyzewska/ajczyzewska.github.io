f4c2f425d2461a6de8e5cf050359358ea34496c1
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2020-06-22 08:36:06 +0200

    Added styling

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,17 +1,26 @@
 const $doc = $(document);
-const $header = $('header');
-const $main = $('main');
-$doc.on('scroll', function(){
-    const scrollPos = $doc.scrollTop();
-    const sectionOffset = $main.offset().top;
-    const headerHeight = $header.outerHeight();
-    console.log(`zmiana`)
-    if (scrollPos< sectionOffset){
-            $header.css({
-                'opacity': 1- scrollPos/headerHeight,
-                'filter': `grayscale(${2 * scrollPos / headerHeight}), contrast(0.3);`,
-            })
-    }
-
-
-})
+const $header = $("header");
+const $main = $("main");
+$doc.on("scroll", function () {
+  const scrollPos = $doc.scrollTop();
+  const sectionOffset = $main.offset().top;
+  const headerHeight = $header.outerHeight();
+  console.log(`zmiana`);
+  if (scrollPos < sectionOffset) {
+    $header.css({
+      opacity: 1 - scrollPos / headerHeight,
+      filter: `grayscale(${(2 * scrollPos) / headerHeight}), contrast(0.3);`,
+    });
+  }
+});
+// SCROLL DOWN 
+const arrow = document.querySelector(".container");
+const porttfolio = document.querySelector("div.portfolio-section");
+arrow.addEventListener("click", (e)=>{
+    console.log("click")
+    porttfolio.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+});
