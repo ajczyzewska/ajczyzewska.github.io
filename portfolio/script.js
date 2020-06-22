6dfc4542613f55ceaab8732630ad5c48ddbb9fb2
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2020-06-22 22:32:50 +0200

    Added Skills & changed orange color

--- portfolio/script.js
+++ portfolio/script.js
@@ -40,3 +40,43 @@ arrow.addEventListener("click", (e)=>{
 });
 
 
+// SKILLS BAR
+jQuery(document).ready(function() {
+  // HTML
+  jQuery(".bar-color").css({
+    "background": "#fd7726cf",
+    "width": "95%"
+  });
+  // CSS
+  jQuery(".skills:nth-of-type(2) .bar-color").css({
+    "width": "80%"
+  });
+  //jQuery
+  jQuery(".skills:nth-of-type(3) .bar-color").css({
+    "width": "70%"
+  });
+  // js
+  jQuery(".skills:nth-of-type(4) .bar-color").css({
+    "width": "55%"
+  });
+  // Cypress
+  jQuery(".skills:nth-of-type(5) .bar-color").css({
+    "width": "75%"
+  });
+  // ReactJS
+  jQuery(".skills:nth-of-type(6) .bar-color").css({
+    "width": "30%"
+  });
+  //Axios
+  jQuery(".skills:nth-of-type(7) .bar-color").css({
+    "width": "55%"
+  });
+  //Figma
+  jQuery(".skills:nth-of-type(8) .bar-color").css({
+    "width": "80%"
+  });
+  //Github
+  jQuery(".skills:nth-of-type(9) .bar-color").css({
+    "width": "80%"
+  });
+});
