5360ee585ab7f98b0d7f3d9a7656655986387441
Agnieszka Czyzewska <agnieszka.j.czyzewska@gmail.com>
2025-10-16 21:52:29 +0200

    Update portfolio page

--- script.js
+++ script.js
@@ -61,42 +61,13 @@ $contact_btn.on('click', function() {
 
 // SKILLS BAR
 jQuery(document).ready(function() {
-  // HTML
-  jQuery(".bar-color").css({
-    "background": "#fd7726cf",
-    "width": "95%"
-  });
-  // CSS
-  jQuery(".skills:nth-of-type(2) .bar-color").css({
-    "width": "80%"
-  });
-  //jQuery
-  jQuery(".skills:nth-of-type(3) .bar-color").css({
-    "width": "70%"
-  });
-  // js
-  jQuery(".skills:nth-of-type(4) .bar-color").css({
-    "width": "55%"
-  });
-  // Cypress
-  jQuery(".skills:nth-of-type(5) .bar-color").css({
-    "width": "75%"
-  });
-  // ReactJS
-  jQuery(".skills:nth-of-type(6) .bar-color").css({
-    "width": "30%"
-  });
-  //Axios
-  jQuery(".skills:nth-of-type(7) .bar-color").css({
-    "width": "55%"
-  });
-  //Figma
-  jQuery(".skills:nth-of-type(8) .bar-color").css({
-    "width": "80%"
-  });
-  //Github
-  jQuery(".skills:nth-of-type(9) .bar-color").css({
-    "width": "80%"
+  // Loop through each skill and set the width based on data attribute
+  jQuery(".skills[data-skill-level]").each(function() {
+    const skillLevel = jQuery(this).data("skill-level");
+    jQuery(this).find(".bar-color").css({
+      "background": "#fd7726cf",
+      "width": skillLevel + "%"
+    });
   });
 });
 
@@ -109,3 +80,29 @@ burger.addEventListener('click', function(){
    burger.classList.toggle('menu-show');
    header.classList.toggle('menu-show');
 });
+
+// TYPING ANIMATION
+const typingText = document.querySelector('.typing-text');
+const cursor = document.querySelector('.cursor');
+const bioText = "Software engineer with over a decade of experience in software development. She is passionate about building great products and is known for her knack for improving even the most well-built systems. A specialist in JavaScript, she is always keen to expand her knowledge and master new technologies.";
+
+let charIndex = 0;
+let typingSpeed = 40; // milliseconds per character
+
+function typeText() {
+  if (charIndex < bioText.length) {
+    typingText.textContent += bioText.charAt(charIndex);
+    charIndex++;
+    setTimeout(typeText, typingSpeed);
+  } else {
+    // Hide cursor after typing is complete
+    setTimeout(() => {
+      cursor.style.display = 'none';
+    }, 2000);
+  }
+}
+
+// Start typing after a delay (to allow header animations to complete)
+setTimeout(() => {
+  typeText();
+}, 3000);
